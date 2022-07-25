3d83f0a415b902141c9cf164f9ce164415b41aff
Shen Yi Chen <kuya5220@gmail.com>
2022-07-25 20:46:35 +0800

    Fixed folder reading bug

--- model/dataset/emotion_processor.py
+++ model/dataset/emotion_processor.py
@@ -15,26 +15,33 @@ def to_Emotion(energy, valence):
     return emotion
 
 def file_emotion_classfier(dir_path):
+    
+    emo_list = ["Happy", "Angry", "Relaxed", "Sad"]
+    pe = 'processed_emotion'
+    if not os.path.exists(pe):
+        os.mkdir(pe)
+        for emo in emo_list:
+            folder = os.path.join(pe, emo)
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+        
     file_dir = os.listdir(dir_path)
 
     print("Total file : ", len(file_dir))
 
     for file in file_dir:
-        file_path = dir_path + '/' + file
+        file_path = os.path.join(dir_path, file)
 
         with open(file_path) as f:
             file_json = json.load(f)
             emotion = to_Emotion(file_json['audio_features']['valence'],
                                  file_json['audio_features']['energy'])
 
-            save_path = 'processed_emotion/' + emotion + '/' + file
+            save_path = os.path.join(pe, emotion, file) 
             with open(save_path, 'w') as out_f:
                 json.dump(file_json, out_f)
-
 
 # %%
 if __name__ == "__main__":
     path = 'processed'
     file_emotion_classfier(path)
-
-# %%
