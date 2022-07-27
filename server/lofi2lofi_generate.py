1e19c6d1af60a9706769fb4d2c56755e72dbeb7f
ShaoTX <shao88014@gmail>
2022-07-27 16:13:28 +0800

    Fixed bug of import

--- server/lofi2lofi_generate.py
+++ server/lofi2lofi_generate.py
@@ -1,7 +1,6 @@
 import torch
 
-from server.output import *
-
+from .output import *
 
 def decode(model, mu):
     hash, (pred_chords, pred_notes, pred_tempo, pred_key, pred_mode, pred_valence, pred_energy) = model.decode(mu)
@@ -9,7 +8,6 @@ def decode(model, mu):
     output = Output(hash, pred_chords, pred_notes, pred_tempo, pred_key, pred_mode, pred_valence, pred_energy)
 
     json = output.to_json(True)
-    print(json)
     return json
 
 
