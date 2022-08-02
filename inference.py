1980858441a4a814bcb25b0a3544b599e8af8d58
ShaoTX <shao88014@gmail>
2022-08-02 16:09:27 +0800

    Can generate with condition

--- inference.py
+++ inference.py
@@ -1,30 +1,48 @@
+#%%
 import torch
 import json
 import jsonpickle
 import argparse
 import os
 
+from model.lofi2lofi_model import idx2onehot
 from model.lofi2lofi_model import Decoder as Lofi2LofiDecoder
 from model.constants import *
 from server.output import Output
 from server.lofi2lofi_generate import *
+import torch.nn as nn
 
 device = "cpu"
-
 
 def inference(weight_path):
     lofi2lofi_checkpoint = weight_path
 
     # print("Loading lofi model...")
     lofi2lofi_model = Lofi2LofiDecoder(device=device)
-    lofi2lofi_model.load_state_dict(torch.load(
-        lofi2lofi_checkpoint, map_location=device))
+    lofi2lofi_model.load_state_dict(torch.load(lofi2lofi_checkpoint, map_location=device))
 
     # print(f"Loaded {lofi2lofi_checkpoint}.")
     lofi2lofi_model.to(device)
 
     lofi2lofi_model.eval()
-    # mu = torch.randn(1, HIDDEN_SIZE)
-    json_out = generate(lofi2lofi_model)
+    # json_out = generate(lofi2lofi_model)
+
+    num = input("Please input a number : ")
+    num = torch.tensor([int(num)]) # [10] -> [1, 10]
+
+    mu = torch.randn(1, HIDDEN_SIZE)
+    label = idx2onehot(num, 4)
+
+    z_condition = torch.cat((mu, label), dim=-1) # [1, 104] , 4 for emotion condition
+    print(z_condition)
+    json_out = decode(lofi2lofi_model, z_condition)
 
     return json_out
+
+#%%
+
+weight_base = os.path.join("model", "weights", "lofi2lofi-decoder-epoch10.pth")
+a = inference(weight_base)
+
+print(a)
+# %%
