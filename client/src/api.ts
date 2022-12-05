81e61c34c5d72e977413449a747ea467c6b60122
huichuanliu@outlook.com <huichuanliu@outlook.com>
2022-12-05 12:00:13 +0800

    redirect client request addr and port

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,11 +1,13 @@
 import { OutputParams } from './params';
 
 export const generate = (): Promise<OutputParams> =>
-  fetch('https://lofiserver.jacobzhang.de/generate')
+  // fetch('https://127.0.0.1:17337/generate')
+  fetch('https://127.0.0.1:17337/generate')
     .then((response) => response.json())
     .then((response) => JSON.parse(response) as OutputParams);
 
 export const decode = (inputList: number[]): Promise<OutputParams> =>
-  fetch(`https://lofiserver.jacobzhang.de/decode?input=${JSON.stringify(inputList)}`)
-    .then((response) => response.json())
+  // fetch(`https://lofiserver.jacobzhang.de/decode?input=${JSON.stringify(inputList)}`)
+  fetch(`http://127.0.0.1:17337/decode?input=${JSON.stringify(inputList)}`)
+  .then((response) => response.json())
     .then((response) => JSON.parse(response) as OutputParams);
